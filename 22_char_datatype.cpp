be847f598812330cdd548a69aa2cbdad2f9caa90
Yash Deshmukh <deshmukhyash442@gmail.com>
2025-06-16 18:21:43 +0530

    DSA CODES

--- 22_char_datatype.cpp
+++ 22_char_datatype.cpp
@@ -2,10 +2,18 @@
 using namespace std;
 
 int main(){
-    char ch = '@';
-    cout<<(int) ch <<endl;
-    int a = 34 ;
-    cout<<(char) a <<endl;
+    // char ch = '@';
+    // cout<<(int) ch <<endl;
+    // int a = 34 ;
+    // cout<<(char) a <<endl;
+
+    char ch[10] = {'c','o','d','e','\0'};
+    cout<<ch<<endl;
+    char cha[10] = "code";
+    cout<< cha <<endl;
+    
+
+    
 
 return 0;
 }
