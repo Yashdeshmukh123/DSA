3a73e0477eeb34d71a0f82bd0adfb15117f2a7e7
Yash Deshmukh <deshmukhyash442@gmail.com>
2025-08-14 13:09:09 +0530

    DSA CODES

--- 35_polymorphism.cpp
+++ 35_polymorphism.cpp
@@ -1,40 +1,86 @@
 #include <iostream>
 using namespace std;
 
-
-class complex{
+class complex
+{
     int real;
     int img;
 
-    public: 
-        complex(int r , int i)
-        {
-            real = r;
-            img = i;
-        }
+public:
+    complex(int r, int i)
+    {
+        real = r;
+        img = i;
+    }
 
-        void shownum(){
-            cout<<real<<"+"<<img<<"i"<<endl;
-        }
+    void shownum()
+    {
+        cout << real << "+" << img << "i" << endl;
+    }
 
-        void operator +(complex &c2){
-            int resreal = this->real + c2.real;
-            int resimg = this->img + c2.img;
-            complex c3(resreal,resimg);
-            cout<<"result = ";
-            c3.shownum();
-        }
+// operator overloading....
+    complex operator+(complex &c2)
+    {
+        int resreal = this->real + c2.real;
+        int resimg = this->img + c2.img;
+        complex c3(resreal, resimg);
+        cout << "result = ";
+        return c3;
+    }
 };
 
-int main(){
+//function overriding....
+//and virtual function...
+
+class parent
+{
+public:
+    void show()
+    {
+        cout << "parent class" << endl;
+    }
+    virtual void print(){
+        cout<<"printing form parent class.."<<endl;
+    }
+};
+
+class child : public parent
+{
+public:
+    void show()
+    {
+        cout << "child class" << endl;
+    }
+    void print(){
+        cout<<"printing from child class";
+    }
+};
+
+
+
+int main()
+{
     complex c1(1,2);
     complex c2(3,4);
-    
+
     c1.shownum();
     c2.shownum();
+
     // c1.operator+(c2);
-    c1+c2;
+    // c1+c2;
 
+    complex c3 = c1 + c2;
+    c3.shownum();
 
-return 0;
+    child c;
+    c.show();
+
+    // virtual funtion call
+    child child1;
+    parent *ptr;
+
+    ptr = &child1;//run time binding
+    ptr->print();//virtual funtion
+
+    return 0;
 }
