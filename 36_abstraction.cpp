fbbe5346b97fbc60b47b3d45a97863fc59f784b1
Yash Deshmukh <deshmukhyash442@gmail.com>
2025-08-19 15:30:00 +0530

    DSA CODES

---
+++ 36_abstraction.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+//abstract class
+class shape{
+    public:
+        virtual void draw() = 0 ;//abstract fnx or pure virtual fnx
+};
+
+class circle : public shape{
+    public:
+        void draw(){
+            cout<<"drawing a circle.."<<endl;
+        }
+};
+
+class square : public shape{
+    public:
+        void draw(){
+            cout<<"drawing a square.."<<endl;
+        }
+};
+
+
+int main(){
+
+    circle c1;
+    c1.draw();
+    
+return 0;
+}
